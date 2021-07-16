028163c69e45c88784cf6c59d648edf0133991ec
Khushi Kapoor <thekhushikapoor@gmail.com>
2021-07-17 00:56:05 +0530

    Done till q5

---
+++ Assignment 3/Question4.java
@@ -0,0 +1,83 @@
+import java.io.*;
+import java.util.*;
+
+public class Question4 {
+
+    public static void main(String[] args) throws Exception {
+        // write your code here
+        Scanner sc = new Scanner(System.in);
+        int r = sc.nextInt();
+        int c = sc.nextInt();
+        int[][] ar = new int[r][c];
+        for(int i = 0; i < r; i++)
+        {
+            for(int j = 0; j < c; j++)
+                ar[i][j] = sc.nextInt();
+        }
+        int total = r * c;
+        int srow = 0;
+        int scol = 0;
+        int erow = r;
+        int ecol = c;
+        int i = 0;
+        int j = 0;
+        while(true)
+        {
+            i = srow;
+            j = scol;
+            while(i < erow)
+            {
+                System.out.println(ar[i][j]);
+                i++;
+                total--;
+            }
+            if(total == 0)
+                break;
+            i--;
+            scol++;
+            j++;
+            while(j < ecol)
+            {
+                System.out.println(ar[i][j]);
+                total--;
+                j++;
+            }
+            if(total == 0)
+                break;
+            j--;
+            erow--;
+            i--;
+            while(i >= srow)
+            {
+                System.out.println(ar[i][j]);
+                total--;
+                i--;
+            }
+            if(total == 0)
+                break;
+            i++;
+            ecol--;
+            j--;
+            while(j >= scol)
+            {
+                System.out.println(ar[i][j]);
+                total--;
+                j--;
+            }
+            if(total == 0)
+                break;
+            srow++;
+        }
+    }
+}
+/*
+Question :-
+
+Take as input a 2-D array. Print it in spiral form anti-clockwise.
+Input: 4 4
+11 12 13 14
+21 22 23 24
+31 32 33 34
+41 42 43 44
+Output: 11, 21, 31, 41, 42, 43, 44, 34, 24, 14, 13, 12, 22, 32, 33, 23
+*/
